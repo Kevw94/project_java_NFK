2fc18bed476fb4e3cca1811ed0cc4da660cd1daa
niko <Gaya9578>
2021-12-08 10:27:17 +0100

    commit for pull

--- src/main/java/com/example/project_java_nfk/HelloController.java
+++ src/main/java/com/example/project_java_nfk/HelloController.java
@@ -3,7 +3,7 @@ package com.example.project_java_nfk;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HelloController  {
     @FXML
     private Label welcomeText;
 
